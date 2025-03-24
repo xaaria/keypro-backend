c57ddce9e584cae19bb73723d57ee582a5b3b6cb
okkop <okkop@invalid.com>
2025-03-24 19:39:43 +0200

    Add DRF to settings.py and authtoken

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # DRF
+    'rest_framework',
+    'rest_framework.authtoken',
     # Own apps here
     'backend',
     'api',
