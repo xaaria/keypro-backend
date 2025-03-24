5c44118cfdd5baa44fa39f87710e1777e1a098c4
okkop <okkop@invalid.com>
2025-03-24 20:07:46 +0200

    Add id, created_by and timestamp fields as readonly in POISerializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 from backend.models import PointOfInterest
 
+# reminder: read_only_fields are not applied during creation
+
 # The most basic POI Serializer
 class POISerializer(serializers.ModelSerializer):
 
     class Meta:
         model = PointOfInterest
         fields = '__all__'
+        read_only_fields = ['id', 'created_by', 'updated_at', 'created_at', 'deleted_at']
