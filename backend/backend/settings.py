ad2367fc0924698c640987e79664cd61afba8746
okkop <okkop@invalid.com>
2025-03-24 13:10:28 +0200

    Add Postgres database config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Own apps here
+    'backend',
     'api',
 ]
 
@@ -76,10 +78,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        # Better approach would be to get these from .env and/or from ENV variables...
+        # Use Dockers service name (here 'db') if using Docker!!!
+        url = "postgres://postgres:postgres@db:5432/postgres",
+        engine='django.db.backends.postgresql',
+        conn_max_age=0,
+        conn_health_checks=False,
+    ),
 }
 
 
@@ -107,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Helsinki'
 
 USE_I18N = True
 
