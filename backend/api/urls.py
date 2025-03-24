a9511af380a9aed4a66ad3e38fb6e80ebfd94d3f
okkop <okkop@invalid.com>
2025-03-24 19:47:05 +0200

    Add Token authentication and GET api/token endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,12 +1,15 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 from . import views
 
 router = DefaultRouter()
 
 urlpatterns = [
     path('pois', views.POIViewSet.as_view(), name='pois'),
+    path('pois/<pk>', views.POIGetSingleViewSet.as_view(), name='pois'),
     path("hello", views.hello, name="hello"),
+    path('token', obtain_auth_token),
     path('', include(router.urls)),
 ]
 
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -138,4 +138,10 @@ REST_FRAMEWORK = {
     'COERCE_DECIMAL_TO_STRING': False,
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 50,
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        #
+    )
 }
