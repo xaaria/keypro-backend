cc6a2c22e4d91a4b196823ce5a95b3843b080d66
okkop <okkop@invalid.com>
2025-03-24 14:33:58 +0200

    Add generic GET endpoint for POIs

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+
 urlpatterns = [
+    path('pois', views.POIViewSet.as_view(), name='pois'),
     path("hello", views.hello, name="hello"),
+    path('', include(router.urls)),
 ]
+
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.generics import GenericAPIView
+from rest_framework import mixins, viewsets
+
+from .serializers import POISerializer
+from backend.models import PointOfInterest
 
 
 def hello(request):
     return HttpResponse("Hello, world")
+
+
+
+class POIViewSet(
+    mixins.ListModelMixin,   
+    GenericAPIView,
+):
+    
+    queryset = PointOfInterest.objects.all()
+    serializer_class = POISerializer
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
